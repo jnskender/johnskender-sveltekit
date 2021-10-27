1f0edfc86b20f329c055b975157175260edda066
jnskender <jnskender@gmail.com>
2021-10-27 11:46:44 -0500

    import articles on homepage

---
+++ src/routes/index.json.js
@@ -0,0 +1,18 @@
+import path from 'path';
+import { getArticles, getArticlesContent } from '$lib/utils/blog';
+
+
+export async function get() {
+  const articlesPath = "src/routes/articles"
+  const __dirname = path.resolve();
+  const location = path.join(__dirname, articlesPath)
+  const postsContent = await getArticlesContent(location);
+  const articles = await getArticles(postsContent);
+
+  console.log(postsContent)
+  return {
+    body: {
+      articles
+    }
+  }
+}

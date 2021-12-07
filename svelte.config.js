a7664da1b26510b73beda155e27f4afa48a960eb
jnskender <jnskender@gmail.com>
2021-12-07 11:39:23 -0600

    add netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
 import { mdsvex } from "mdsvex";
+import adapter from '@sveltejs/adapter-netlify';
 import mdsvexConfig from "./mdsvex.config.js";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   "extensions": [".svelte", ...mdsvexConfig.extensions],
   kit: {
+    adapter: adapter(),
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte'
   },  layout: "./src/components/ArticleLayout.svelte",

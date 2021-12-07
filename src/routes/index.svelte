f29152fc520c804d208a71de7a36a2ee1722757a
jnskender <jnskender@gmail.com>
2021-12-07 13:41:07 -0600

    add /.netlify to gitignore

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,8 +3,7 @@
 	 * @type {import('@sveltejs/kit').Load}
 	 */
 	export async function load({ fetch }) {
-		const url = `./index.json`;
-		const response = await fetch(url);
+		const response = await fetch('./index.json');
 
 		if (response.ok) {
 			return {

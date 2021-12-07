2e0158d70f8a8f83c67d63a01281f5e2dffab621
jnskender <jnskender@gmail.com>
2021-12-07 17:59:57 -0600

    back to netlify lol

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
 import { mdsvex } from "mdsvex";
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-netlify';
 import mdsvexConfig from "./mdsvex.config.js";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   "extensions": [".svelte", ...mdsvexConfig.extensions],
   kit: {
-    adapter: vercel(),
+    adapter: adapter(),
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte'
   },

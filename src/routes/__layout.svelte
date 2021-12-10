442b4bd9c161526fc4ae2a167f2ecaf1081d6e9e
jnskender <jnskender@gmail.com>
2021-12-09 21:02:10 -0600

    optimize fonts

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,6 +7,15 @@
 	import '../styles/app.css';
 </script>
 
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
 <NavBar />
 <Page>
 	<slot />

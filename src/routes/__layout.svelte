12155883611dff68d3645ead9c1d4b25d0cc87b1
jnskender <jnskender@gmail.com>
2022-01-15 08:51:29 -0600

    use self-hosted fonts

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,18 +2,14 @@
 	import NavBar from '$lib/components/NavBar.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import Page from '$lib/components/Page.svelte';
+	import '@fontsource/inter';
+	import '@fontsource/fira-code';
 	import '../styles/reset.css';
 	import '../styles/app.css';
 	import '../styles/prism.css';
 </script>
 
 <svelte:head>
-	<link rel="preconnect" href="https://fonts.googleapis.com" />
-	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-	<link
-		href="https://fonts.googleapis.com/css2?family=Fira+Code&family=Inter:wght@400;600;700;800&display=swap"
-		rel="stylesheet"
-	/>
 	<!-- Global site tag (gtag.js) - Google Analytics -->
 	<script async src="https://www.googletagmanager.com/gtag/js?id=G-SVLYV7EXZM"></script>
 	<script>

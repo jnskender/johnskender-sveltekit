b0e3e4a1ab46fb52614b806f928b20f8a291f950
jnskender <jnskender@gmail.com>
2022-02-04 09:41:32 -0600

    remove target from svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,6 @@ const config = {
     },
     adapter: adapter(),
     // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte',
     vite: {
       resolve: {
         alias: {

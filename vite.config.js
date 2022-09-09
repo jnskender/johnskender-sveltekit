1bd55f6ddbd0fa16ab1ba2a6a6212f46ffb3eec1
jnskender <jnskender@gmail.com>
2022-09-09 15:06:09 -0500

    remove unnecessary resolve

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 import { imagetools } from 'vite-imagetools';
-import path from 'path';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-  resolve: {
-    alias: {
-      $static: path.resolve('src/static')
-    }
-  },
   plugins: [imagetools({ force: true }), sveltekit()]
 }
 

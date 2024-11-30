9f216d2517a06663b94fe0f444a38db3b6a72bef
jnskender <jnskender@gmail.com>
2024-11-30 10:14:13 -0600

    allow multi extensions and only show published

--- src/routes/articles/+page.svelte
+++ src/routes/articles/+page.svelte
@@ -1,7 +1,8 @@
 <script>
 	import ArticlesList from '$lib/components/ArticlesList.svelte';
-	const articles = Object.values(import.meta.glob('../articles/*.svx', { eager: true }))
+	const articles = Object.values(import.meta.glob('../articles/*.{svx,md}', { eager: true }))
 		.map((article) => article.metadata)
+		.filter((article) => article.isPublished)
 		.sort((a, b) => new Date(b.createdOn) - new Date(a.createdOn)); // most recent on the top
 </script>
 
